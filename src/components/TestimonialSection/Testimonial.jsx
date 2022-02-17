5bf64f4268872eea2f7d463252f17ab26d91c302
Lord-Mugen <christianjavieruizcortes@gmail.com>
2022-02-17 12:37:26 -0500

    Ultimo cambio hero

--- src/components/TestimonialSection/Testimonial.jsx
+++ src/components/TestimonialSection/Testimonial.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faStar} from '@fortawesome/free-solid-svg-icons'
 import './Testimonial.css'
@@ -25,29 +24,6 @@ const Testimonial = () => {
             className="c-testimonial_icon"/>
           </div>
         </div> 
-=======
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faStar } from "@fortawesome/free-solid-svg-icons"
-import "./Testimonial.css"
-
-const Testimonial = () => {
-  return <>
-      <div className="c-testimonial_d">
-        <div className="c-testimonial_contenedor"><h3 className="c-testimonial_tittle">Opiniones de nuestros usuarios</h3></div>
-        <section className="c-testimonial">
-          <div className="c-testimonial_div">
-              <p>
-              ¡Buen servicio! Realmente necesitaba que mi perro saliera a pasear.
-              </p>
-              <div>
-              <FontAwesomeIcon icon={faStar} className="c-testimonial_icon" />
-              <FontAwesomeIcon icon={faStar} className="c-testimonial_icon" />
-              <FontAwesomeIcon icon={faStar} className="c-testimonial_icon" />
-              <FontAwesomeIcon icon={faStar} className="c-testimonial_icon" />
-              <FontAwesomeIcon icon={faStar} className="c-testimonial_icon" />
-              </div>
-          </div>
->>>>>>> 1acdab6716956ebdb7d91342ba6010bc89258d77
 
           <div className="c-testimonial_div">
               <p>
@@ -62,7 +38,6 @@ const Testimonial = () => {
               </div>
           </div>
 
-<<<<<<< HEAD
         <div className="c-testimonial_div"><p>¡Buen servicio!
         Realmente necesitaba que mi perro saliera a pasear.</p>
           <div>
@@ -114,37 +89,6 @@ const Testimonial = () => {
       </div>
 
     </section>
-=======
-          <div className="c-testimonial_div">
-              <p>
-              ¡Buen servicio! Realmente necesitaba que mi perro saliera a pasear.
-              </p>
-              <div>
-              <FontAwesomeIcon icon={faStar} className="c-testimonial_icon" />
-              <FontAwesomeIcon icon={faStar} className="c-testimonial_icon" />
-              <FontAwesomeIcon icon={faStar} className="c-testimonial_icon" />
-              <FontAwesomeIcon icon={faStar} className="c-testimonial_icon" />
-              <FontAwesomeIcon icon={faStar} className="c-testimonial_icon" />
-              </div>
-          </div>
-
-          <div className="c-testimonial_div">
-              <p>
-              ¡Buen servicio! Realmente necesitaba que mi perro saliera a pasear.
-              </p>
-              <div>
-              <FontAwesomeIcon icon={faStar} className="c-testimonial_icon" />
-              <FontAwesomeIcon icon={faStar} className="c-testimonial_icon" />
-              <FontAwesomeIcon icon={faStar} className="c-testimonial_icon" />
-              <FontAwesomeIcon icon={faStar} className="c-testimonial_icon" />
-              <FontAwesomeIcon icon={faStar} className="c-testimonial_icon" />
-              </div>
-          </div>
-          </section>
-
-      </div>
-  </>
->>>>>>> 1acdab6716956ebdb7d91342ba6010bc89258d77
 }
 
 export default Testimonial
