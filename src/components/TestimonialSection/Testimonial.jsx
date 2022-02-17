6ecd6c440fffaac676be785df37e5a29633732ab
Lord-Mugen <christianjavieruizcortes@gmail.com>
2022-02-17 12:25:58 -0500

    responsive opinions

--- src/components/TestimonialSection/Testimonial.jsx
+++ src/components/TestimonialSection/Testimonial.jsx
@@ -3,75 +3,79 @@ import {faStar} from '@fortawesome/free-solid-svg-icons'
 import './Testimonial.css'
 const Testimonial = () => {
     return <section className="c-testimonial">
-         <div className="c-testimonial_contenedor">
-         <h1 className="c-testimonial_tittle">Opiniones de nuestros usuarios</h1>
-         </div> 
 
+      <h1 className="c-testimonial_tittle">Opiniones de nuestros usuarios</h1>
+      
 
-         <div className="c-testimonial_div"><p>¡Buen servicio!
+      <div className="c-testimonial__content-text">
+
+        <div className="c-testimonial_div"><p>¡Buen servicio!
         Realmente necesitaba que mi perro saliera a pasear.</p>
-        <div>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        </div>
+          <div>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+          </div>
         </div> 
 
 
         <div className="c-testimonial_div"><p>¡Buen servicio!
         Realmente necesitaba que mi perro saliera a pasear.</p>
-        <div>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        </div>
-        </div>
-
-        <div className="c-testimonial_div"><p>¡Buen servicio!
-        Realmente necesitaba que mi perro saliera a pasear.</p>
-       <div>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        </div>
+          <div>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+          </div>
         </div>
 
         <div className="c-testimonial_div"><p>¡Buen servicio!
         Realmente necesitaba que mi perro saliera a pasear.</p>
-        <div>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
-        <FontAwesomeIcon icon={faStar}
-        className="c-testimonial_icon"/>
+          <div>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+          </div>
         </div>
+
+        <div className="c-testimonial_div"><p>¡Buen servicio!
+        Realmente necesitaba que mi perro saliera a pasear.</p>
+          <div>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+            <FontAwesomeIcon icon={faStar}
+            className="c-testimonial_icon"/>
+          </div>
         </div>
+
+      </div>
+
     </section>
 }
 
