4fb69d30018ebf4aa9e01cccce5e2e8f7fa02324
Lord-Mugen <christianjavieruizcortes@gmail.com>
2022-02-17 09:33:32 -0500

    ajustes para responsive landingpage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+
 import './App.css';
 import { Route, Routes } from 'react-router-dom'
 
 import Home from './views/Home/Home'
 import Navbar from './components/Navbar/Navbar' // llamado al componente.
 import Hero from './components/Hero/Hero';
+import Testimonial from './components/TestimonialSection/Testimonial'
 
 function App() {
   return <>
@@ -13,6 +15,8 @@ function App() {
       <Route exact path='/' element={<Home/>}/>
       <Route exact path='/home' element={<Home/>}/>
     </Routes>
+    <Testimonial/>
+    
   </>
 }
 
