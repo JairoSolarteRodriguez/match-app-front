ff7052055115b01618ea43e882457559baed2edc
SergiFuentes <you@example.com>
2022-02-17 21:04:07 +0100

    setting testimonial user comments

--- src/components/TestimonialSection/Testimonial.jsx
+++ src/components/TestimonialSection/Testimonial.jsx
@@ -9,8 +9,7 @@ const Testimonial = () => {
 
       <div className="c-testimonial__content-text">
 
-        <div className="c-testimonial_div"><p>¡Buen servicio!
-        Realmente necesitaba que mi perro saliera a pasear.</p>
+        <div className="c-testimonial_div"><p>¡Genial! Encontré justo la persona ideal para hacer tai-chí.</p>
           <div>
             <FontAwesomeIcon icon={faStar}
             className="c-testimonial_icon"/>
@@ -38,8 +37,7 @@ const Testimonial = () => {
               </div>
           </div>
 
-        <div className="c-testimonial_div"><p>¡Buen servicio!
-        Realmente necesitaba que mi perro saliera a pasear.</p>
+        <div className="c-testimonial_div"><p>La aplicación esta muy bien, pude encontar una persona que me ayudó con mi mudanza.</p>
           <div>
             <FontAwesomeIcon icon={faStar}
             className="c-testimonial_icon"/>
@@ -54,13 +52,8 @@ const Testimonial = () => {
           </div>
         </div>
 
-        <div className="c-testimonial_div"><p>¡Buen servicio!
-        Realmente necesitaba que mi perro saliera a pasear.</p>
+        <div className="c-testimonial_div"><p>Lo que le falta son más personas para encontrar coincidencias, pero vale la pena.</p>
           <div>
-            <FontAwesomeIcon icon={faStar}
-            className="c-testimonial_icon"/>
-            <FontAwesomeIcon icon={faStar}
-            className="c-testimonial_icon"/>
             <FontAwesomeIcon icon={faStar}
             className="c-testimonial_icon"/>
             <FontAwesomeIcon icon={faStar}
@@ -70,8 +63,7 @@ const Testimonial = () => {
           </div>
         </div>
 
-        <div className="c-testimonial_div"><p>¡Buen servicio!
-        Realmente necesitaba que mi perro saliera a pasear.</p>
+        <div className="c-testimonial_div"><p>Si no fuera por MatchUp mi madre no podría salir acompañada a dar paseos por las mañanas, gracias Paco!!!.</p>
           <div>
             <FontAwesomeIcon icon={faStar}
             className="c-testimonial_icon"/>
