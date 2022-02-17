e7d64f92b029c25062221da5a6cae74e55c2dd7d
JairoSolarteRodriguez <pvprandom578@gmail.com>
2022-02-17 03:48:22 -0500

    merge whit dev branch

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import { Route, Routes } from 'react-router-dom'
 
 import Home from './views/Home/Home'
 import Navbar from './components/Navbar/Navbar'
-import About from './views/About/About';
-import Hero from './components/Hero/Hero';
+import About from './views/About/About'
 
 function App() {
   return <>
     <Navbar/>
-    <Hero />
     <Routes>
       <Route exact path='/' element={<Home/>}/>
       <Route exact path='/home' element={<Home/>}/>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import './Hero.css'
 import imageHero from './undraw_time_management_re_tk5w.svg'
+
 const Hero = () => {
     return <div className="c-hero">
         <h1 className= "c-hero__title">Yo tengo tiempo,<br/> 
