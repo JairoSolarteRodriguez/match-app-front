75bef722c04d73601a5b6525b65af7281b579195
JairoSolarteRodriguez <pvprandom578@gmail.com>
2022-02-17 20:05:32 -0500

    merge readme

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import { Route, Routes } from 'react-router-dom'
 
 import Home from './views/Home/Home'
 import Navbar from './components/Navbar/Navbar' // llamado al componente.
-import Testimonial from './components/TestimonialSection/Testimonial'
+import TestimonialSection from './components/TestimonialSection/Testimonial'
 import About from './views/About/About'
 import Footer from './components/Footer/Footer'
-import Form from "./components/Form/Form";
 import IndividualAd from "./views/IndividualAd/IndividualAd";
 
 function App() {
@@ -19,7 +18,7 @@ function App() {
       <Route exact path='/home/:idUrl' element={<IndividualAd/>}/>
       <Route exact path='/about' element={<About/>}/>
     </Routes>
-    <Testimonial/>
+    <TestimonialSection/>
     <Footer/>
     
   </>
